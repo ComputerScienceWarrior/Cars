09f8931f5b18e0067a1213110f56f1d7c254efe2
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-22 12:46:58 -0700

    Add logic for index, show, create and update actions. Add logic for find_team and team_params

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -4,33 +4,46 @@ module Api
             before_action :find_team, to: [:show, :update, :destroy]
 
             def index
-
+                @teams = Team.all
+                render json: @teams
             end
 
             def show
-
+                render json: @team
             end
 
             def create
-
+                if @team.save(team_params)
+                    render json: @team
+                else
+                    render json: { error: @team.errors.messages }, status: 422
+                end
             end
 
             def update
-
+                if @team.update(team_params)
+                    render json: @team
+                else
+                    render json: { error: @course.errors.messages }, status: 422
+                end
             end
 
             def destroy
-
+                if @team.destroy
+                    head :no_content
+                else
+                    render json: { error: @course.errors.messages }, status: 422
+                end
             end
 
             private 
 
             def team_params
-
+                params.require(:team).permit(:name, :total_wins, :trophies, cars_attributes: [:name, :wins, :losses, :attack_power, :rank, :speed, :level])
             end
 
             def find_team
-
+                @team = Team.find(params[:id])
             end
         end
     end
