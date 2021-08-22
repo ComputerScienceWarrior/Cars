6f81daa07e1550ab9d1847eb2f3e958911c23cf6
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-22 08:13:15 -0700

    add routes for api/v1 namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root 'static#home'
+
+  namespace :api do
+    namespace :v1 do 
+      resources :teams, only: [:index, :show, :create, :update, :destroy] do
+        resources :cars, only: [:index, :show, :create, :update, :destroy]
+      end
+    end
+  end
+
+  # get '*path', to: :all, via: 'static#home'
 end
