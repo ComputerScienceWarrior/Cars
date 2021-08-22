bd2beec0cb8425884a4bcbdc5e0db7ab8f6a486e
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-22 08:14:54 -0700

    create controller inside ap1/v1 module and [:index, :show, :create, :update, :destroy] action stubs

---
+++ app/controllers/api/v1/cars_controller.rb
@@ -0,0 +1,37 @@
+module Api
+    module V1
+        class CarsController < ApplicationController
+            before_action :find_car, to: [:show, :update, :destroy]
+            
+            def index
+
+            end
+
+            def show
+
+            end
+
+            def create
+
+            end
+
+            def update
+
+            end
+
+            def destroy
+
+            end
+
+            private 
+
+            def car_params
+
+            end
+
+            def find_car
+                
+            end
+        end
+    end
+end
---
+++ app/controllers/api/v1/teams_controller.rb
@@ -0,0 +1,37 @@
+module Api
+    module V1
+        class TeamsController < ApplicationController
+            before_action :find_team, to: [:show, :update, :destroy]
+
+            def index
+
+            end
+
+            def show
+
+            end
+
+            def create
+
+            end
+
+            def update
+
+            end
+
+            def destroy
+
+            end
+
+            private 
+
+            def team_params
+
+            end
+
+            def find_team
+
+            end
+        end
+    end
+end
