b8fad4a36dfaf4f855d04ba272da06f1e979d73d
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-22 08:27:36 -0700

    add logic to actions and private methods.

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -2,35 +2,48 @@ module Api
     module V1
         class CarsController < ApplicationController
             before_action :find_car, to: [:show, :update, :destroy]
-            
-            def index
 
+            def index
+                @cars = Car.all
+                render json: @cars
             end
 
             def show
-
+                render json: @car
             end
 
             def create
-
+                if @car.save(car_params)
+                    render json: @car
+                else
+                    render json: { error: @car.errors.messages }, status: 422
+                end
             end
 
             def update
-
+                if @car.update(car_params)
+                    render json: @car
+                else
+                    render json: { error: @car.errors.messages }, status: 422
+                end
             end
 
             def destroy
-
+                if @car.destroy
+                    head: :no_content
+                else
+                    render json: { error: @car.errors.messages }, status: 422
+                end
             end
 
             private 
 
             def car_params
-
+                params.require(:car).permit(:name, :wins, :losses, :attack_power, :speed, :level, :rank, :team_id)
             end
 
             def find_car
-                
+                @car = Car.find(params[:id])
             end
         end
     end
