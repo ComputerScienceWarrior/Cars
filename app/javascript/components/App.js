fcbaabf5e5c1d45384f4ef5124f5cac8e346808e
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-23 13:20:30 -0700

    Create App.js with Switch and Routing

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+const App = () => {
+    return(
+        <Switch>
+            {/* <Route exact path="/teams" component={Teams}/> */}
+            {/* <Route exact path="/cars" component={Cars}/> */}
+        </Switch>
+    )
+}
+
+export default App;
