8d5995935683458be1eef0b85ffea2a3b8cc969d
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-24 09:20:06 -0700

    import Car and Cars components to App and create Route for Cars index page

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import Cars from '../components/Car/Cars'
+import Car from './Car/Car'
 
 const App = () => {
     return(
         <Switch>
-            {/* <Route exact path="/teams" component={Teams}/> */}
-            {/* <Route exact path="/cars" component={Cars}/> */}
+            <Route exact path="/" component={Cars}/>
+            {/* <Route exact path="/cars/:id/" component={Car}/> */}
         </Switch>
     )
 }
