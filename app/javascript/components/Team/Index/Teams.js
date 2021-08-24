f21b1037fc5e9445876c302f34b1ad8bf7943398
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-24 09:42:09 -0700

    Create Teams component and use axios to get Rails API data to display.

---
+++ app/javascript/components/Team/Index/Teams.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Team from './Team'
+
+const Teams = () => {
+
+    const [teams, setTeams] = useState([])
+
+    useEffect(()=>{
+        axios.get('/api/v1/teams.json')
+        .then(resp => {
+            setTeams(resp.data)
+        })
+        .catch(resp => console.log(resp))
+    }, [teams.length])
+
+    const teamsList = teams.map(team =>{
+        return (
+            <Team
+                name={team.name}
+                total_wins={team.total_wins}
+                trophies={team.trophies}
+            />
+        )
+    })
+    return (
+        <section>
+            <header>This is the index page for Teams</header>
+            {teamsList}
+        </section>
+    )
+}
+
+export default Teams;
