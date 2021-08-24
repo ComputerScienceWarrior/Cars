0429bb9e5f8b7d85e0bfd5c5b8aec8bc6f7df7e7
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-08-24 13:20:40 -0700

    Add ShowCar component and route for a Car show page.

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Cars from '../components/Car/Index/Cars'
 import Teams from '../components/Team/Index/Teams'
+import ShowCar from './Car/Show/ShowCar'
 
 const App = () => {
     return(
         <Switch>
             <Route exact path="/" component={Teams}/> {/*this will change to a main screen form or something similar */}
             <Route exact path="/cars/" component={Cars}/> {/* Index Route */}
+            <Route exact path="/cars/:id/" component={ShowCar}/> {/* Show Route */}
             <Route exact path="/teams/" component={Teams}/> {/* Index Route */}
         </Switch>
     )
